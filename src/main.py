ee3501c11d77a3b54bd4b9e06299d955ed90bd76
devtiagoabreu <devtiagoabreu@gmail.com>
2025-06-21 17:02:39 -0300

    Propriedades de redimensionamento da Janela

--- src/main.py
+++ src/main.py
@@ -6,7 +6,28 @@ def main(page: ft.Page):
   page.window.title_bar_hidden = False
   page.window.frameless = False
   page.window.bgcolor = ft.Colors.TRANSPARENT
-  
+  page.window.full_screen = False
+  page.window.height = 500
+  page.window.max_height = 800
+  page.window.min_height = 300
+  page.window.width = 500
+  page.window.max_width = 800
+  page.window.min_width = 300
+  page.window.resizable = True
+  page.window.movable = False
+  #page.window.top = 100
+  #page.window.left = 100
+  page.window.center()
+
+  def page_resize(e):
+    print("Tamanho:", page.window.height, page.window.width)
+
+  page.on_resized = page_resize
+
+
+  print(page.platform)
+
+
   page.bgcolor = ft.Colors.TRANSPARENT
 
   page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
