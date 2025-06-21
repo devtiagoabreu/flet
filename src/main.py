95abeeda677077e451fce15a24644a3cf895f63e
devtiagoabreu <devtiagoabreu@gmail.com>
2025-06-21 18:24:42 -0300

    Propriedades de texto

--- src/main.py
+++ src/main.py
@@ -1,6 +1,39 @@
 import flet as ft
 
 def main(page: ft.Page):
+  page.fonts = {
+    'kanit': 'https://raw.githubusercontent.com/google/fonts/master/ofl/kanit/Kanit-Bold.ttf',
+    'blankScript': 'fonts/Blank_Script.otf',
+  }
+
+  t1 = ft.Text(
+    value = "Teste de texto!!!",
+    style = ft.TextThemeStyle.DISPLAY_LARGE,
+    #bgcolor = ft.Colors.WHITE,
+    color = ft.Colors.AMBER,
+    font_family = 'blankScript',
+    max_lines = 2,
+    overflow = ft.TextOverflow.ELLIPSIS,
+    selectable = True,
+    size = 50,
+    text_align = ft.TextAlign.JUSTIFY,
+    weight = ft.FontWeight.BOLD,
+
+  )
+
+  link_style = ft.TextStyle(color = ft.Colors.BLUE)
+
+  t2 = ft.Text(
+    spans = [
+      ft.TextSpan(text='wow ', style = link_style),
+      ft.TextSpan(text='wow ', url='http://wow.com.br'),
+      ft.TextSpan(text='wow '),
+    ]
+
+  )
+
+  page.add(t1,t2)
+
   
   page.window.always_on_top = True
   page.window.title_bar_hidden = False
@@ -48,4 +81,4 @@ def main(page: ft.Page):
   page.update()
 
 if __name__ == '__main__':
-  ft.app(target = main)
+  ft.app(target = main, assets_dir = 'assets')
