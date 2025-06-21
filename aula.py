7dc8fad83f6b9bd05a1a94dccf1aa99c70770d78
devtiagoabreu <devtiagoabreu@gmail.com>
2025-06-21 15:44:12 -0300

    propriedades da page

--- aula.py
+++ aula.py
@@ -1,7 +1,24 @@
 import flet as ft
 
 def main(page: ft.Page):
-  mensagem = ft.Text(value='Olá Mundo!')
-  page.add(mensagem)
+  page.bgcolor = "#d6b108"
+
+  page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
+  page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+  page.padding = ft.padding.all(100)
+  page.spacing = 100
+  page.title = 'Flet App'
+
+  page.add(
+    ft.Text(value = 'Olá mundo!'),
+    ft.Container(ft.Text(value='Olá Mundo'), bgcolor='black')
+  )
+
+
+
+
+
+  page.update()
 
 ft.app(target = main)  
